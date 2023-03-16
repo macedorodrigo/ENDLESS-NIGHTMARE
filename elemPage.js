51425aa436f2804a8fdc2f746bdcc2921cba2f8a
macedorodrigo <romaco1694@gmail.com>
2023-03-16 16:56:30 -0300

    Criando button

--- elemPage.js
+++ elemPage.js
@@ -16,8 +16,19 @@ const paragraph = (text) => {
   document.body.appendChild(paragraph);
 }
 
+const button = (text) => {
+  const button = document.createElement('button');
+  button.innerHTML = text;
+  button.setAttribute('type', 'submit'); // Define o tipo do botão como "submit"
+  button.setAttribute('name', 'submit-button'); // Define o nome do botão como "submit-button"
+  button.setAttribute('disabled', ''); // Desabilita o botão
+  button.style.color = 'red'; // Define a cor do texto do botão como vermelho
+  document.body.appendChild(button);
+}
+
 export {
   title,
   subTile,
   paragraph,
+  button,
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { title, subTile, paragraph } from "./elemPage.js"
+import { title, subTile, paragraph, button } from "./elemPage.js"
 
 const main = () => {
   paragraph("the horror game of the year")
   title("ENDLESS<br>NIGHTMARE")
   subTile("You need to find the way out or your night will never end!")
+  button("READ MORE")
+  button("PLAY NOW")
 }
 
 main()
